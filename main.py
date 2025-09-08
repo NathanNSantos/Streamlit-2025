355a57818d1d945ccf97a0e8eb18aa2b8ff22895
NathanNSantos <nathansantos2500@gmail.com>
2025-09-08 15:35:28 -0300

    08-09

--- main.py
+++ main.py
@@ -2,37 +2,65 @@ import streamlit as st
 import pandas as pd
 
 
+def calc_general_stats(df):
+    # Agrupa os dados por Data e mantém em formato DataFrame
+    df_data = df.groupby("Data", as_index=True)[["Valor"]].sum()
+
+    # Criação de colunas auxiliares
+    df_data["lag_1"] = df_data["Valor"].shift(1)
+    df_data["Diferença Mensal Abs."] = df_data["Valor"] - df_data["lag_1"]
+
+    # Médias móveis
+    df_data["Média 6M Diferença Mensal Abs."] = df_data["Diferença Mensal Abs."].rolling(6).mean()
+    df_data["Média 12M Diferença Mensal Abs."] = df_data["Diferença Mensal Abs."].rolling(12).mean()
+    df_data["Média 24M Diferença Mensal Abs."] = df_data["Diferença Mensal Abs."].rolling(24).mean()
+
+    # Diferença relativa
+    df_data["Diferença Mensal Rel."] = (df_data["Valor"] / df_data["lag_1"]) - 1
+
+    # Evoluções absolutas
+    df_data["Evolução 6M Total"] = df_data["Valor"].rolling(6).apply(lambda x: x[-1] - x[0])
+    df_data["Evolução 12M Total"] = df_data["Valor"].rolling(12).apply(lambda x: x[-1] - x[0])
+    df_data["Evolução 24M Total"] = df_data["Valor"].rolling(24).apply(lambda x: x[-1] - x[0])
+
+    # Evoluções relativas
+    df_data["Evolução 6M Relativa"] = df_data["Valor"].rolling(6).apply(lambda x: x[-1]/x[0] - 1)
+    df_data["Evolução 12M Relativa"] = df_data["Valor"].rolling(12).apply(lambda x: x[-1]/x[0] - 1)
+    df_data["Evolução 24M Relativa"] = df_data["Valor"].rolling(24).apply(lambda x: x[-1]/x[0] - 1)
+
+    # Remove a coluna auxiliar
+    df_data = df_data.drop("lag_1", axis=1)
+
+    return df_data
+
+
+# Configuração da página
 st.set_page_config(page_title="Finanças", page_icon='💰')
 
 st.markdown("""
-            
 # Seja Bem-vindo
-            
 ## APP Financeiro
-            
-            
-    """)
+""")
 
-#Widget de Upload de arquivos
-file_upload=st.file_uploader(label="Faça Upload dos dados aqui", type=["csv"])
+# Upload de arquivos
+file_upload = st.file_uploader(label="Faça Upload dos dados aqui", type=["csv"])
 
 if file_upload:
 
-    #Leitura dos dados
+    # Leitura dos dados
     df = pd.read_csv(file_upload)
     df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y", errors="coerce").dt.date
-  
 
-    #Exibição dos dados
+    # Exibição dos dados brutos
     exp1 = st.expander('Dados Brutos')
-    columns_fnt = {"Valor": st.column_config.NumberColumn("Valor",format="$%.2f")}
+    columns_fnt = {"Valor": st.column_config.NumberColumn("Valor", format="R$%.2f")}
     exp1.dataframe(df, hide_index=True, column_config=columns_fnt)
 
-    #Visão Instituição
+    # Visão Instituições
     exp2 = st.expander('Instituições')
     df_instituicao = df.pivot_table(index="Data", columns="Instituição", values="Valor")
-    
-    tab_data, tab_history, tab_share = exp2.tabs(["Dados","Histórico","Distribuição"])
+
+    tab_data, tab_history, tab_share = exp2.tabs(["Dados", "Histórico", "Distribuição"])
 
     with tab_data:
         st.dataframe(df_instituicao)
@@ -41,10 +69,97 @@ if file_upload:
         st.line_chart(df_instituicao)
 
     with tab_share:
-
+        # Filtro de Data
         date = st.selectbox("Filtro Data", options=df_instituicao.index)
+        # Gráfico de distribuição
+        st.bar_chart(df_instituicao.loc[date])
 
-        #Obtém a última data de dados
-        last_dt = df_instituicao.sort_index().iloc[date]
-        st.bar_chart(last_dt)
 
+    exp3=st.expander("Estatísticas Gerais")
+    # Estatísticas gerais
+    df_stats = calc_general_stats(df)
+
+    # Configuração de colunas formatadas
+    columns_config = {
+       "Valor": st.column_config.NumberColumn("Valor", format="R$%.2f"),
+       "Diferença Mensal Abs.": st.column_config.NumberColumn("Diferença Mensal Abs.", format="R$%.2f"),
+       "Média 6M Diferença Mensal Abs.": st.column_config.NumberColumn("Média 6M Diferença Mensal Abs.", format="R$%.2f"),
+       "Média 12M Diferença Mensal Abs.": st.column_config.NumberColumn("Média 12M Diferença Mensal Abs.", format="R$%.2f"),
+       "Média 24M Diferença Mensal Abs.": st.column_config.NumberColumn("Média 24M Diferença Mensal Abs.", format="R$%.2f"),
+       "Diferença Mensal Rel.": st.column_config.NumberColumn("Diferença Mensal Rel.", format="%.2f%%"),
+       "Evolução 6M Total": st.column_config.NumberColumn("Evolução 6M Total", format="R$%.2f"),
+       "Evolução 12M Total": st.column_config.NumberColumn("Evolução 12M Total", format="R$%.2f"),
+       "Evolução 24M Total": st.column_config.NumberColumn("Evolução 24M Total", format="R$%.2f"),
+       "Evolução 6M Relativa": st.column_config.NumberColumn("Evolução 6M Relativa", format="%.2f%%"),
+       "Evolução 12M Relativa": st.column_config.NumberColumn("Evolução 12M Relativa", format="%.2f%%"),
+       "Evolução 24M Relativa": st.column_config.NumberColumn("Evolução 24M Relativa", format="%.2f%%"),
+    }
+
+    tab_stats, tab_abs, tab_rel = exp3.tabs(tabs=("Dados","Histórico de Evolução", "Crescimento Relativos"))
+
+    with tab_stats:
+        st.dataframe(df_stats, column_config=columns_config)
+
+    with tab_abs:
+        abs_cols=["Diferença Mensal Abs.", "Média 6M Diferença Mensal Abs.", "Média 12M Diferença Mensal Abs.", "Média 24M Diferença Mensal Abs."]
+        st.line_chart(df_stats[abs_cols])
+
+    with tab_rel:
+        rel_cols=["Diferença Mensal Rel.", "Evolução 6M Relativa", "Evolução 12M Relativa", "Evolução 24M Relativa" ]
+        st.line_chart(df_stats[rel_cols])
+
+
+    with st.expander("📌 Metas"):
+
+        col1, col2 = st.columns(2)
+
+        data_inicio_meta = col1.date_input(
+            "📅 Início da Meta",
+            max_value=df_stats.index.max()
+        )
+        data_filtrada = df_stats.index[df_stats.index <= data_inicio_meta][-1]
+
+        custos_fixos = col1.number_input(
+            "💸 Custos Fixos",
+            min_value=0.0,
+            format="%.2f"
+        )
+        col1.markdown(f"**Custos Fixos:** R$ {custos_fixos:,.2f}")
+
+        salario_bruto = col2.number_input(
+            "💵 Salário Bruto",
+            min_value=0.0,
+            format="%.2f"
+        )
+        col2.markdown(f"**Salário Bruto:** R$ {salario_bruto:,.2f}")
+
+        salario_liq = col2.number_input(
+            "💰 Salário Líquido",
+            min_value=0.0,
+            format="%.2f"
+        )
+        col2.markdown(f"**Salário Líquido:** R$ {salario_liq:,.2f}")
+
+        # --- Valor inicial ---
+        valor_inicio = df_stats.loc[data_filtrada]["Valor"]
+        col1.markdown(f"**Valor no Início da Meta:** R$ {valor_inicio:,.2f}")
+
+        # --- Potenciais ---
+        col1_pot, col2_pot = st.columns(2)
+        mensal = salario_liq - custos_fixos
+        anual = mensal * 12
+
+        with col1_pot.container(border=True):
+            st.markdown(f"📈 **Potencial Arrecadação Mensal:**\n\n R$ {mensal:,.2f}")
+
+        with col2_pot.container(border=True):
+            st.markdown(f"📊 **Potencial Arrecadação Anual:**\n\n R$ {anual:,.2f}")
+
+        # --- Meta estipulada ---
+        meta_estipulada = st.number_input(
+            "🎯 Meta Estipulada",
+            min_value=0.0,
+            format="%.2f",
+            value=anual
+        )
+        st.markdown(f"**Meta Estipulada:** R$ {meta_estipulada:,.2f}")
